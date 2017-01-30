94a73268d566c14a4fef377bf183e8e47249d333
chiragghevariya <gcb1196@gmail.com>
2017-01-30 07:18:45 +0530

    department_id and semester collumn added successfully

--- database/migrations/2017_01_30_012238_add_sem_into_users.php
+++ database/migrations/2017_01_30_012238_add_sem_into_users.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddDepartmentIdIntoUsers extends Migration
+class AddSemIntoUsers extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,7 @@ class AddDepartmentIdIntoUsers extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->integer('department_id')->default(0);
+            $table->integer('semester')->default(0);
         });
     }
 
@@ -26,7 +26,7 @@ class AddDepartmentIdIntoUsers extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('department_id');
+            $table->dropColumn('semester');
         });
     }
 }
