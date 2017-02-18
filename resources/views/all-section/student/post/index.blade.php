d5b22e629f8a7f6755cd9b0a8b51571241a0f902
chiragghevariya <gcb1196@gmail.com>
2017-02-18 16:58:38 +0530

    like ,comment share and cardview for profile are set

--- resources/views/all-section/student/post/index.blade.php
+++ resources/views/all-section/student/post/index.blade.php
@@ -13,6 +13,19 @@
 
 
             }
+            .post-description{
+
+                font-family:"Lucida Sans Unicode", "Lucida Grande", sans-serif;
+                margin-left: 1em;
+                font-weight: 400;
+                font-size: medium;
+                background: white;
+                padding: 1em;
+            }
+            .icon{
+
+                margin-right: 1em;
+            }
 
         </style>
 
@@ -28,47 +41,63 @@
 
 
 
+        <div class="row" style="width: 50%;margin: auto">
 
+            @foreach($post as $post)
 
-                @foreach($post as $post)
+            <div class="col-sm-12">
 
-                <div class="row">
+                <div class="panel panel-default">
 
-                    <div class="panel panel-default">
+                    <div class="panel-heading panelheading">
+                        <div class="row">
+                            <div class="col-sm-2">
 
-                            <div class="panel-heading panelheading">
-
-                                <img width="40" height="40"  src="{{url('Profile/'.$post->user_id.'/images/'.App\Profile::where('user_id',$post->user_id)->value('photo'))}}" alt="No" class="img-responsive img-circle profile-image">
-                                    {{App\User::Name($post->user_id)}}
-
-
-
-                            </div>
-
-                            <div class="panel-body">
-
-                                <img width="100%" height="100%" src="{{url('Post/'.$post->user_id. '/images/'.$post->photo)}}">
-
-                                <hr>
-
-                                <div style=" text-align:center">{{$post->description}}</div>
+                                <img src="{{url('Profile/'.$post->user_id.'/images/'.App\Profile::where('user_id',$post->user_id)->value('photo'))}}" alt="No" class="img-responsive img-circle profile-image">
 
                             </div>
-
-                            <hr>
-
-                            <div class="panel-footer bg-success"  >
-
-                                <h6 style=" text-align: center">{{$post->created_at->diffForHumans()}}</h6>
+                            <div class="col-sm-5">
+                                <b> {{App\User::Name($post->user_id)}}</b><br>
+                                <h5 style="font-size:smaller">{{$post->created_at->diffForHumans()}}</h5>
 
                             </div>
-
                         </div>
 
                     </div>
 
 
+                    <div class="post-description">{{$post->description}}</div>
+
+
+                    <div class="panel-body">
+
+                        <img width="100%" height="100%" src="{{url('Post/'.$post->user_id. '/images/'.$post->photo)}}">
+
+                    </div>
+
+                    <div class="panel-footer">
+
+                        <div class="row" style="text-align: center">
+
+                            <div class="col-md-4"><span class="icon"><i class="fa fa-thumbs-up" aria-hidden="true"></i></span>Like</div>
+                            <div class="col-md-4"><span class="icon"><i class="fa fa-comment" aria-hidden="true"></i></span>Comment</div>
+                            <div class="col-md-4"><span class="icon"><i class="fa fa-share-alt" aria-hidden="true"></i></span>Share</div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+
+            </div>
+
             @endforeach
+
+
+        </div>
+
+
             </div>
 
 
